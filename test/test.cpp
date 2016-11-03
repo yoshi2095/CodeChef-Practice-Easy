a10ad62b7b719c486c254efd72c7b2e3099602c8
Yoshita Arora <arora4.yoshi@gmail.com>
2016-11-03 19:40:25 +0530

    3 easy problems done!

---
+++ test/test.cpp
@@ -0,0 +1,20 @@
+    #include<iostream>
+    #include<stdio.h>
+    using namespace std;
+    int main()
+    { int a;
+     
+      while(1)
+      {
+         cin>>a;
+         if(a==42)
+         {
+             break;
+         }
+         cout<<a<<endl;
+     
+      }
+     
+    return 0;
+     
+    } 
